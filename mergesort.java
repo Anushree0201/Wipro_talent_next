ccc5cb9a53de1a2858c2521211cea78bf107bee3
Anushree@0201 <anuanushab999gmail.com>
2025-07-30 18:29:09 +0530

    TestRunner

--- mergesort.java
+++ mergesort.java
@@ -1,39 +1,40 @@
-// program to take 2 array list of size is dynamic use user input and merge them and later sort them
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
-
-class mergesort {
+class ArrayListDemo {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        ArrayList<Integer> list1 = new ArrayList<>();
-        ArrayList<Integer> list2 = new ArrayList<>();
-
-        System.out.print("Enter size of first array: ");
-        int size1 = sc.nextInt();
-        System.out.println("Enter elements of first array:");
-        for (int i = 0; i < size1; i++) {
-            list1.add(sc.nextInt());
+        Scanner s = new Scanner(System.in);
+        // Create an ArrayList to hold String elements
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
+        System.out.println("Enter an integer for list1: (5 integers)");
+        for(int i = 0; i < 5; i++) {
+            
+            int input1 = s.nextInt();
+            list1.add(input1);
         }
-
-        System.out.print("Enter size of second array: ");
-        int size2 = sc.nextInt();
-        System.out.println("Enter elements of second array:");
-        for (int i = 0; i < size2; i++) {
-            list2.add(sc.nextInt());
+        // list1.add(10);
+        // list1.add(20);
+        // list1.add(30);
+        System.out.println("Enter an integer for list2:(3 integers)");
+        for(int i = 0; i < 3; i++) {
+            
+            int input1 = s.nextInt();
+            list2.add(input1);
         }
+        // list2.add(11);
+        // list2.add(22);
+        // list2.add(33);
 
-        ArrayList<Integer> mergedList = mergeLists(list1, list2);
+        int size1 = list1.size();
+        int size2 = list2.size();
+
+        ArrayList<Integer> mergedList = new ArrayList<>(size1 + size2);
+        mergedList.addAll(list1);
+        mergedList.addAll(list2);
         System.out.println("Merged List: " + mergedList);
         
-        mergedList.sort(null); // Sort the merged list
+        mergedList.sort(null);
         System.out.println("Sorted Merged List: " + mergedList);
-        
-        sc.close();
-    }
-
-    public static ArrayList<Integer> mergeLists(ArrayList<Integer> list1, ArrayList<Integer> list2) {
-        ArrayList<Integer> merged = new ArrayList<>(list1);
-        merged.addAll(list2);
-        return merged;
     }
 }
